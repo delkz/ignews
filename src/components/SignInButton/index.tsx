8348e0d8b6af30c21fa0bc32c56ee46f9624bc81
David Silva <davidlsilva.sp@gmail.com>
2022-07-05 13:49:26 -0300

    SignInButton

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,19 @@
+import styles from "./styles.module.scss";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#04d361" />
+      David Silva
+      <FiX color="#737380" className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#eba417" />
+      Sign in with github
+    </button>
+  );
+}
