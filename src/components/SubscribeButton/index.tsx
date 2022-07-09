8a2328057461f7e9d6bad63b50e1aeef292125ba
David Silva <davidlsilva.sp@gmail.com>
2022-07-09 12:35:14 -0300

    Pagamentos no stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,5 +8,27 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}:SubscribeButtonProps) {
-  return (<button type="button" className={styles.subscribeButton}>Subscribe now</button>);
+
+  const { data: session } = useSession();
+  async function handleSubscribe(){
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    
+    try{
+      const response = await api.post('/subscribe')
+      const {sessionId} = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId});
+    } catch (error){
+      alert(error.message);
+    }
+  }
+
+
+  return (<button onClick={handleSubscribe} type="button" className={styles.subscribeButton}>Subscribe now</button>);
 }
