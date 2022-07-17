3ac57a396278fb6d3dc4ce2947d4106d3e29679c
David Silva <davidlsilva.sp@gmail.com>
2022-07-16 23:46:45 -0300

    Menu Mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,62 @@
-import Link from 'next/link';
-import { ActiveLink } from '../ActiveLink';
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
-export function Header(){
-
-    return(
-      <header className={styles.headerContainer}>
-        <div className={styles.headerContent}>
-            <img src="/images/logo.svg" alt="ig.news" />
+import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+import { FiX } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import { FaBars } from "react-icons/fa";
+export function Header() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+  useEffect(() => {
+    const clientWidth = document.documentElement.clientWidth;
+    if (clientWidth <= 768)
+      setIsMobile(true);
+  }, [])
+  
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href={"/"}>
+          <img src="/images/logo.svg" alt="ig.news" />
+        </Link>
+        {isMobile ? (
+          <button className={styles.mobileMenu} onClick={()=>{setMobileMenuIsOpen(!mobileMenuIsOpen)}}><FaBars/></button>
+        ) : (
+          <>
             <nav>
               <ActiveLink activeClassName={styles.active} href="/">
                 <a>Home</a>
               </ActiveLink>
-              <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+              <ActiveLink
+                activeClassName={styles.active}
+                href="/posts"
+                prefetch
+              >
                 <a>Posts</a>
               </ActiveLink>
-                
             </nav>
-            <SignInButton/>
-        </div>
-      </header>  
-    );
+            <SignInButton />
+          </>
+        )}
+      </div>
+      <div
+        className={`${styles.headerMobile} ${mobileMenuIsOpen ? styles.headerMobileActive : ""}`}
+      >
+        <button className={styles.closeMenuButton} onClick={()=>{setMobileMenuIsOpen(!mobileMenuIsOpen)}}>
+          <FiX color="#737380" className={styles.closeIcon} />
+        </button>
+        <SignInButton />
+        <nav>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+      </div>
+      <div className={styles.menuBackdrop}></div>
+    </header>
+  );
 }
