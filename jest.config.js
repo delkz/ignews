9d61c327e9cbec4adc2f1d7ad2377e7d95081540
David Silva <davidlsilva.sp@gmail.com>
2023-01-05 15:17:31 -0300

    test: configuração inicial

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+    testIgnorePatterns: ['/node_modules/','/.next/'],
+    setupFilesAfterEnv:[
+        "<rootDir>/src/tests/setupTests.ts"
+    ],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$":"<rootDir>"
+    },
+    testEnvironment: 'jsdom'
+}
